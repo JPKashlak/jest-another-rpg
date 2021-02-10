787df61b588567b53076a8f5e6f7cf869a724314
jpkashlak <jpkashlak@gmail.com>
2021-02-09 18:35:54 -0600

    tested & added player health stats

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,6 +4,8 @@ console.log(new Potion());
 
 const Player = require('../lib/Player');
 const { expect, test } = require('@jest/globals');
+const { ensureExpectedIsNonNegativeInteger } = require('jest-matcher-utils');
+const expectExport = require('expect');
 
 
 
@@ -36,4 +38,33 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+})
+
+test('checks in player is alive or not', () => {
+    const player = new Player ('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+    
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
